9b157255234ceca75b154efdcebfb7d7f5fec54b
ljayoon <ljayoon@gmail.com>
2026-08-31 22:05:15 +0900

    feat: animate siege commander movement

--- tests/entities/Enemy.test.ts
+++ tests/entities/Enemy.test.ts
@@ -1,6 +1,6 @@
 import type Phaser from 'phaser';
 import {
-  Enemy, fastWalkFrameAt, normalWalkFrameAt, regeneratorWalkFrameAt,
+  bossWalkFrameAt, Enemy, fastWalkFrameAt, normalWalkFrameAt, regeneratorWalkFrameAt,
   shieldWalkFrameAt, summonerWalkFrameAt, tankWalkFrameAt,
 } from '../../src/entities/Enemy';
 import type { EnemyDef } from '../../src/core/types';
@@ -90,6 +90,12 @@ describe('regenerator / summoner walk animation', () => {
   });
   it('summoner drifts on a four-frame cycle', () => {
     expect([0, 204, 205, 410, 615, 820].map(summonerWalkFrameAt)).toEqual([0, 0, 1, 2, 3, 0]);
+  });
+});
+
+describe('siege commander walk animation', () => {
+  it('loops four command poses while the boss advances', () => {
+    expect([0, 179, 180, 360, 540, 720].map(bossWalkFrameAt)).toEqual([0, 0, 1, 2, 3, 0]);
   });
 });
 
